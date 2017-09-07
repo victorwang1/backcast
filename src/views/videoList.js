a8e0e245cbf931370860fe8c816f64dc9ee3cfcb
victorwang1 <victor.wang@me.com>
2017-09-06 17:34:19 -0700

    passed first test

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -4,7 +4,12 @@ var VideoListView = Backbone.View.extend({
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.collection.forEach(this.renderList, this);
     return this;
+  },
+  renderList: function(entry){
+    var videoListEntry = new VideoListEntryView({model: entry});
+    this.$el.append(videoListEntry.render());
   },
 
   template: templateURL('src/templates/videoList.html')
